3fd945880da1e0dc3ef1016088bd8304f796c33f
Bogdan Sadriev <bogda077077@gmail.com>
2024-09-23 18:22:34 +0400

    reactor store file

--- src/store/store.ts
+++ src/store/store.ts
@@ -11,6 +11,7 @@ export const store = configureStore({
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(notesApi.middleware),
+  devTools: process.env.NODE_ENV !== "production",
 });
 
 export type RootState = ReturnType<typeof store.getState>;
